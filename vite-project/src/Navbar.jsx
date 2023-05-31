1ab16f75ed56a64a9e591a0b1f155536d2405ec6
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-05-31 17:03:45 +0700

    feat-responsive-design

--- vite-project/src/Navbar.jsx
+++ vite-project/src/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="navbar">
+      <nav className="toggleNav">
+        <button className="toggleBtn" onClick={toggleMenu}>
+          {/* <img src="./home_icon.PNG" className=" bgBtn"/> */}
+        </button>
+        {isOpen && (
+          <nav className="tNav">
+            <ul className="menu">
+              <li className="menuItem">
+                <a href="#profile">About</a>
+              </li>
+              <li className="menuItem">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="menuItem">
+                <a href="#experience">Experience</a>
+              </li>
+              <li className="menuItem">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </nav>
       <nav className="siteTitle">
         <a className="home" href="#profile">
           Haluethaichanok Junlongsawaitkul
